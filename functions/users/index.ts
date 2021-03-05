7e839338b8a752eca6c9d4ef383b2a92553bd93a
cool-develope <cool199966@outlook.com>
2021-03-05 14:20:00 +0200

    Added users function

---
+++ functions/users/index.ts
@@ -0,0 +1,58 @@
+// @ts-ignore
+import * as AWS from 'aws-sdk';
+import { Handler } from 'aws-lambda';
+import { Client } from '@elastic/elasticsearch';
+import { FunctionEvent, FieldName, UsersConnection } from './types';
+
+const { MAIN_TABLE_NAME, ES_DOMAIN } = process.env;
+const db = new AWS.DynamoDB.DocumentClient();
+const es = new Client({ node: ES_DOMAIN });
+
+export const handler: Handler = async (event: FunctionEvent): Promise<UsersConnection> => {
+  const {
+    arguments: { filter = {}, limit = 10, from = 0 },
+    identity: { sub, claims },
+    info: { fieldName },
+  } = event;
+
+  if (fieldName === FieldName.users) {
+    /**
+     * TODO: Implement search in Elasticsearch
+     */
+    
+    return {
+      items: [
+        {
+          id: '1',
+          email: 'test@gmail.com',
+          firstName: 'Sergey',
+          lastName: 'Onufrienko',
+          parentUserId: null,
+          country: 'UA',
+          city: '',
+          phone: '+3867890987',
+          phoneCountry: '+38',
+          birthDate: new Date(1985, 1, 1).toISOString(),
+          createDate: new Date().toISOString(),
+          treezor: {
+            userId: 456789,
+            walletId: 456789,
+            kycReview: 'Pending',
+          },
+          recentActivity: [
+            {
+              ip: '127.0.0.1',
+              country: 'UA',
+              city: '',
+              device: 'Android',
+              loginDate: new Date().toISOString(),
+            },
+          ],
+        },
+      ],
+      totalCount: 1,
+    };
+  }
+
+  throw Error('Query not supported');
+};
---
+++ functions/users/types.ts
@@ -0,0 +1,64 @@
+export enum FieldName {
+  users = 'users',
+}
+
+export interface FunctionEvent {
+  arguments: {
+    filter?: any;
+    limit?: number;
+    from?: number;
+  };
+  identity: { sub: string; claims: CognitoClaims };
+  info: { fieldName: FieldName };
+}
+
+export interface CognitoClaims {
+  sub: string;
+  aud: string;
+  token_use: string;
+  email: string;
+  'cognito:groups': string[];
+  'cognito:username': string;
+  'custom:trzUserId': string;
+  'custom:clubs': string;
+  'custom:federations': string;
+  'custom:trzWalletsId': string;
+  'custom:trzScopes': string;
+  'custom:trzCardsId': string;
+  'custom:trzChildren': string;
+}
+
+export interface TreezorUser {
+  userId: number | null;
+  walletId: number | null;
+  kycReview: string | null;
+}
+
+export interface LoginActivity {
+  ip: String;
+  country: String;
+  city: string;
+  device: string;
+  loginDate: string;
+}
+
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  parentUserId: string | null;
+  country: string;
+  city: string;
+  phone: string;
+  phoneCountry: string;
+  birthDate: string;
+  createDate: string;
+  treezor: TreezorUser;
+  recentActivity: LoginActivity[];
+}
+
+export interface UsersConnection {
+  items: User[];
+  totalCount: number;
+}
